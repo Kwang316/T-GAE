9f942095a1bead73237cc43d8a1597bc585bad78
Kwang316 <149460411+Kwang316@users.noreply.github.com>
2024-12-27 18:29:47 -0800

    Update graphMatching.py

--- graphMatching.py
+++ graphMatching.py
@@ -61,7 +61,7 @@ def main(args):
 
     if args.mapping_only:
         print("Loading trained model...")
-        model = TGAE(input_dim=1, hidden_dim=16, output_dim=8, num_hidden_layers=8).to(device)
+        model = TGAE(input_dim=1, hidden_dim=[16] * 8, output_dim=8, num_hidden_layers=8).to(device)
         model.load_state_dict(torch.load(args.load_model))
         model.eval()
         print("Loaded trained model. Computing mapping...")
