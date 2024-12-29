72c9bab87d9bc47f80bc6a4756450ea4e9eba7f4
Kwang316 <149460411+Kwang316@users.noreply.github.com>
2024-12-29 00:39:01 -0800

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class GINConv(torch.nn.Module):
@@ -9,37 +10,35 @@ class GINConv(torch.nn.Module):
 
     def forward(self, A, X):
         X = self.linear(X + A @ X)
-        X = torch.nn.functional.relu(X)
-        return X
+        return F.relu(X)
 
 
-class TGAE_Encoder(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, n_layers):
+class TGAE_Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_hidden_layers):
         super().__init__()
-        hidden_layers = n_layers - 2
-        self.in_proj = torch.nn.Linear(input_dim, hidden_dim[0])
-        self.convs = torch.nn.ModuleList()
-        for i in range(hidden_layers):
-            self.convs.append(GINConv(input_dim + hidden_dim[i], hidden_dim[i + 1]))
-        self.out_proj = torch.nn.Linear(sum(hidden_dim), output_dim)
+        if len(hidden_dim) < num_hidden_layers - 2:
+            raise ValueError(f"`hidden_dim` must have at least {num_hidden_layers - 2} elements.")
+
+        self.in_proj = nn.Linear(input_dim, hidden_dim[0])
+        self.convs = nn.ModuleList(
+            GINConv(input_dim + hidden_dim[i], hidden_dim[i + 1]) for i in range(num_hidden_layers - 2)
+        )
+        self.out_proj = nn.Linear(sum(hidden_dim), output_dim)
 
     def forward(self, A, X):
-        initial_X = torch.empty_like(X).copy_(X)
         X = self.in_proj(X)
         hidden_states = [X]
-        for layer in self.convs:
-            X = layer(A, torch.cat([initial_X, X], dim=1))
+        for conv in self.convs:
+            X = conv(A, torch.cat([hidden_states[0], X], dim=1))
             hidden_states.append(X)
         X = torch.cat(hidden_states, dim=1)
-        X = self.out_proj(X)
-        return X
+        return self.out_proj(X)
 
 
-class TGAE(torch.nn.Module):
+class TGAE(nn.Module):
     def __init__(self, num_hidden_layers, input_dim, hidden_dim, output_dim):
         super().__init__()
-        self.encoder = TGAE_Encoder(input_dim, hidden_dim, output_dim, num_hidden_layers + 2)
+        self.encoder = TGAE_Encoder(input_dim, hidden_dim, output_dim, num_hidden_layers)
 
     def forward(self, X, adj):
-        Z = self.encoder(adj, X)
-        return Z
+        return self.encoder(adj, X)
